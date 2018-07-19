f2b31d84e911a3f683fcaba8fe020cde92308335
Ivakin Eugene <yevgeniy.ivakin@catenamedia.com>
2018-07-19 12:29:26 +0300

    backup

---
+++ markup/src/assets/js/one-page-nav.js
@@ -0,0 +1,56 @@
+jQuery(function($) {
+    var $menuHolder = $('#menu'),
+        menu = [],
+        sectionId = 0;
+
+    $('.content-holder').find('h1,h2,h3,h4,h5').each(function(n) {
+        var $el = $(this);
+        var id = Date.now() + n;
+        $el.attr('id', id);
+
+        if ($el.is('h2') || $el.is('h1')) {
+            sectionId = id;
+            menu[sectionId] = {
+                id: id,
+                text: $el.text(),
+                items: []
+            }
+        }
+        else if (sectionId){
+            menu[sectionId].items[id] = {
+                id: id,
+                text: $el.text(),
+                items: []
+            }
+        }
+    });
+
+    if (Object.keys(menu)) {
+        var $menu = buildMenuFromArray(menu);
+        $menuHolder.append($menu);
+        
+    }
+
+    // initAnchors();
+
+});
+
+
+
+function buildMenuFromArray(menu) {
+    if (Object.keys(menu) == 0) return;
+    var $menu = $('<ul/>');
+    for (var menuKey in menu) {
+        var menuItem = menu[menuKey];
+        var $menuItem = $('<li></li>');
+        var $submenu = buildMenuFromArray(menuItem.items);
+        var $menuItemContent = $('<a href="#' + menuItem.id + '">' + menuItem.text + '</a>');
+        if ($submenu) {
+            $menuItemContent = $('<div class="opener-holder"></div>').append($menuItemContent).append('<a href="#" class="opener"></a>');
+        }
+        $menuItem.append($menuItemContent);
+        $menuItem.append($submenu);
+        $menu.append($menuItem);
+    };
+    return $menu;
+}
